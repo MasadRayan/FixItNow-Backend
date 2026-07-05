856f6fb6070d0534f133e4ebd7bb2506934eb604
Masad Rayan <masadrayan2002@gmail.com>
2026-07-05 23:22:17 +0600

    created all the model as per the design (we will cahnge things if we need anything)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const main = async () => {
     try {
-        app.listen(3000, () => {
-        console.log("Server is running on port 3000")
+        await prisma.$connect();
+        console.log("Connected to the database")
+        app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`)
     })
     } catch (error: any) {
         console.log("Error Starting in the serevr" , error);
+        await prisma.$disconnect();
         process.exit(1);
     }
 }
